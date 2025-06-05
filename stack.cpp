711cd1228a4cb3f90c2f279688f1934435aaddde
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-06-05 10:40:33 +0700

    menambahkan fungsi push untuk menambahkan elemen ke stack

--- stack.cpp
+++ stack.cpp
@@ -28,4 +28,15 @@ class Stack
     {
         top = NULL;
     }
+
+
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
 }
