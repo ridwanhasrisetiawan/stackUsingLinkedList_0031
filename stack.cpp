52f1fbd0220cba8390a9457e0da832b5195b260a
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-06-05 11:15:08 +0700

    tambahkan case exit dan validasi pilihan menu

--- stack.cpp
+++ stack.cpp
@@ -106,6 +106,13 @@ int main()
             case 3:
             stack.peek();
             break;
+
+            case 4:
+            cout << "Exiting program." << endl;
+            break;
+            default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
         }
     }
 }
